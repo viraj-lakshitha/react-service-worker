d5ca3e80cac69bf2438702a43a036b0bed6594cb
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-08-08 16:32:01 +0530

    Install bootstrap and create basic script

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Service Worker
+          </a>
+        </div>
+      </nav>
+      <div className='container-fluid'>
+        Service worker with React JS
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
